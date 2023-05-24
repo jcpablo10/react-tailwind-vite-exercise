f51beed5b74d0608d8ac7be93463d66b016db722
Juan Carlos Pablo <juan.pablo@palaceresorts.com>
2023-05-24 11:43:27 -0600

    Added functions and states to context

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,13 +5,27 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({ children }) => {
 
+  // Cart count
   const [count, setCount] = useState(0)
+
+  // Side Product Detail * Open Close
+  const [isProductDetailOpen, setIsProductDetailOpen] =  useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
+
+  // Product
+  const [selectedProduct, setSelectedProduct] = useState(null)
 
   return (
     // Crear un proveedor que encapsula a los compoenentes y proveerles la info
     <ShoppingCartContext.Provider value={{
       count,
-      setCount
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      setSelectedProduct,
+      selectedProduct,
     }}>
       {children}
     </ShoppingCartContext.Provider>
