8759c3736b831e5783a99540c959a34e0942e9c4
Juan Carlos Pablo <juan.pablo@palaceresorts.com>
2023-05-24 11:40:56 -0600

    Open close side view

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,12 +1,21 @@
 import { useContext } from "react"
 import {ShoppingCartContext} from '../../Context/index'
+import { PlusIcon } from "@heroicons/react/24/outline";
 
-const Card = ({category, images, price, title}) => {
+const Card = ({category, images, price, title, description}) => {
 
-  const {count, setCount} = useContext(ShoppingCartContext)
+  const {count, setCount, openProductDetail, setSelectedProduct} = useContext(ShoppingCartContext)
+
+  const selectProduct = (product) => {
+    openProductDetail()
+    setSelectedProduct(product)
+  }
 
   return (
-    <div className="bg-white cursor-pointer w-56 h-60">
+    <div
+      onClick={() => selectProduct({category, images, price, title, description})}
+      className="bg-white cursor-pointer w-56 h-60"
+    >
       <figure className="relative mb-2 w-full h-4/5">
         <span
           className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5"
@@ -21,7 +30,7 @@ const Card = ({category, images, price, title}) => {
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
           onClick={() => setCount(count + 1)}
         >
-          +
+          <PlusIcon className="w-6 h-6" />
         </div>
       </figure>
       <p className="flex justify-between items-center">
